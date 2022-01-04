265d3803287cf97b0f71630464adab3ab9a98506
Taha Khan <tahak734@gmail.com>
2022-01-04 16:18:23 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Taha Khan on 1/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
